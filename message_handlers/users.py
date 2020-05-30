ff7509109c6f0b388e155c4ee75c3fe2ba219988
Astemir Unarokov <40520443+ast3310@users.noreply.github.com>
2020-05-30 14:28:23 +0300

    Update users.py

--- message_handlers/users.py
+++ message_handlers/users.py
@@ -14,7 +14,7 @@ import json
 from helpers.carousel import Carousel
 
 class AddUserHandler(MessageBaseHandler):
-    permissions = [isAdmin]
+    permissions = []
 
     def check(self, message):
         command, _ = self.parse_command(message.text)
